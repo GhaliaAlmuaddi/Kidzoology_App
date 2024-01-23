46bd9219af2815fb3c53892cef564d5522280104
rahafibrahim21 <rahafibrahim999@gmail.com>
2024-01-23 10:04:18 +0300

    Initial Commit

---
+++ AnimalGame/AnimalGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalGameApp.swift
+//  AnimalGame
+//
+//  Created by Rahaf ALghuraibi on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct AnimalGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimalGame
+//
+//  Created by Rahaf ALghuraibi on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
