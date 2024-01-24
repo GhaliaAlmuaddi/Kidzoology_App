5821c89cdc4aff342f91e9bb93144520562b23ea
maryam16mgh <mmgh16@gmail.com>
2024-01-24 11:10:43 +0300

    commit

--- AnimalGame/AnimalsModel.swift
+++ AnimalGame/AnimalsModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+struct Animals {
+    var animalName : String
+    var type : String
+    var diet : String
+    var habitat : String
+    var groupName : String
+    var fact : String
+    var animalImage : String
+    
+}
+
+
+var camel = Animals(animalName: "Camel", type: "Mammals", diet: "Herbivore", habitat: "Deserts", groupName: "Caravan or flock", fact: "Have one or two hump stores fat break to water when sustenance is not available and can conserve water for long of time.", animalImage: "Camel")
+
+var horse = Animals(animalName: "Horse", type: "Mammals", diet: "Herbivore", habitat: "Steppes and open Grasslands", groupName: "Herd", fact: "The horse is fast, so it has a long leg bone , and has a set of strong, high-crowned teeth, suited to grinding grasses and other harsh vegetation.", animalImage: "Horse")
+
+var sheep = Animals(animalName: "Sheep", type: "Mammals", diet: "Herbivore", habitat: "Hilly or Rangelands", groupName: "Herd", fact: "Sheep can jump nearly 12 feet in a single bound , have distinctive beards and long, warm coats to protect them from cold temperatures and biting mountain winds.", animalImage: "Sheep")
+
+var cow = Animals(animalName: "Cow", type: "Mammals", diet: "Herbivore", habitat: "Grassland", groupName: "Herd or drove", fact: "Cows Produce milk that we use  to make butter, yogurt and many other products. A baby cow is called a calf . An adult male is called a bull.", animalImage: "Cow")
+
+var gazelle = Animals(animalName: "Gazelle", type: "Mammals", diet: "Herbivore", habitat: "Deserts and Grasslands", groupName: "Herd", fact: "Gazelle can be extremely fast because there are dangerous predators they need to escape from. like Cheetahs, lions.", animalImage: "Gazelle")
+
+var wolf = Animals(animalName: "Wolf", type: "Mammals", diet: "Carnivore", habitat: "Forests and Mountains", groupName: "Pack", fact: "Wolves communicate with more than howls. The howl is used as a way to call to another pack or warn of danger. Each pack has a unique howl.", animalImage: "Wolf")
+
+var dog = Animals(animalName: "Dog", type: "Mammals", diet: "Omnivore", habitat: "Prairies and Deserts.", groupName: "Pack", fact: "Dogs can provide assistance to humans by performing various tasks such as hunting, guarding, and searching for missing persons.", animalImage: "Dog")
+
+var pigeon = Animals(animalName: "Pigeon", type: "Bird", diet: "Seeds and Grains", habitat: "Diverse environments worldwide", groupName: "Flock or Flight", fact: "They usually make nests of sticks, and eggs are incubated parent. Unlike most other birds, the Columbidae produce a type of milk and they feed their young with it.", animalImage: "Pigeon")
+
+var hawk = Animals(animalName: "Hawk", type: "Bird of Prey", diet: "Carnivorous", habitat: "Mountains", groupName: "Kettle", fact: "Hawk are birds of prey, they hunt and eat animals for food. have sharp claws and a short hooked beak help it catch and kill animals.", animalImage: "Hawk")
+
+var cat = Animals(animalName: "Cat", type: "Mammal", diet: "Carnivorous", habitat: "Diverse Habitats", groupName: "Clowder", fact: "Cats conserve energy by sleeping for 13 to14 hours a day. and have flexible bodies and teeth adapted for hunting small animals such as mice and rats.", animalImage: "Cat")
+
+var sparrows = Animals(animalName: "Sparrows", type: "Bird", diet: "Omnivorous and seeds", habitat: "Rural environments", groupName: "Flocks (groups)", fact: "Sparrows often make their nests near houses or buildings. This means they are one of the easiest birds to see in the wild.", animalImage: "Bird")
+
+var snake = Animals(animalName: "Snake", type: "Reptile", diet: "Carnivorous", habitat: "Deserts and Forests", groupName: "Den", fact: "The snake's body is covered with scales molts or sheds its skin many times during its life , and have no ears or eyelids.", animalImage: "Snake")
+
+var scorpion = Animals(animalName: "Scorpion", type: "Arachnid", diet: "Carnivorous", habitat: "Warm and dry Regions", groupName: "bed of scorpions", fact: "Scorpions use their poison to kill prey and to defend against predators. They quickly grab an insect with their pincers and sting their prey.", animalImage: "Scorpion")
+
+var lizard = Animals(animalName: "Lizard", type: "Reptile", diet: "Varied", habitat: "Deserts and Rainforests", groupName: "Lounge", fact: "Lizards are cold-blooded animals. This means that the temperature of the body changes based on the temperature of the surroundings.", animalImage: "Lizard")
