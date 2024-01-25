2a55af8bec6267c849a612dc89855b31aa0d09dc
maryam16mgh <mmgh16@gmail.com>
2024-01-25 11:52:25 +0300

    animal facts

---
+++ AnimalGame/AnimalFactView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalFactView.swift
+//  AnimalGame
+//
+//  Created by Maryam Mohammad on 13/07/1445 AH.
+//
+
+import SwiftUI
+
+struct AnimalFactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalFactView()
+}
