210b4a041c160b72b26b128fcf8b120e47e4c9a7
shomokh123 <ip5115@outlook.sa>
2024-01-28 11:38:11 +0300

    animals list and info

---
+++ AnimalGame/sucessMassage.swift
@@ -0,0 +1,18 @@
+//
+//  sucessMassage.swift
+//  AnimalGame
+//
+//  Created by shomokh aldosari on 16/07/1445 AH.
+//
+
+import SwiftUI
+
+struct sucessMassage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sucessMassage()
+}
