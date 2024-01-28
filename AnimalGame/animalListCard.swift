ab197ededcac0030f4a438dd37993ec54b3ca84a
shomokh123 <ip5115@outlook.sa>
2024-01-28 09:50:40 +0300

    model

---
+++ AnimalGame/animalListCard.swift
@@ -0,0 +1,18 @@
+//
+//  animalListCard.swift
+//  AnimalGame
+//
+//  Created by shomokh aldosari on 16/07/1445 AH.
+//
+
+import SwiftUI
+
+struct animalListCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    animalListCard()
+}
