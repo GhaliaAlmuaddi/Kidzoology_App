70e6b21f0103c34a012a01d610ceb16ffe613e07
shomokh123 <ip5115@outlook.sa>
2024-02-02 12:37:46 +0300

    all sounds

--- AnimalGame/SoundsModel.swift
+++ AnimalGame/SoundsModel.swift
@@ -1,31 +1,5 @@
 //
-//  SoundsModel.swift
-//  AnimalGame
-//
-//  Created by Maryam Mohammad on 19/07/1445 AH.
-//
 
-//import SwiftUI
-//import AVFoundation
-//
-//
-//var audioPlayer: AVAudioPlayer?
-//
-//func playSound(sound: String){
-//    if let path = Bundle.main.path(forResource: sound, ofType: "mp3"){
-//        do{
-//            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-//            audioPlayer?.play()
-//        } catch {
-//            print("ERROR, Couldn't find and play the sound")
-//        }
-//    }
-//}
-//
-//func stopAudio(){
-//    audioPlayer?.stop()
-//    audioPlayer = nil
-//}
 
 import SwiftUI
 import AVFoundation
@@ -53,7 +27,7 @@ private func localizedSoundFileName(for sound: String) -> String {
     let languageCode = Locale.current.languageCode
     var localizedSoundName = sound
     
-    // Check the current language code and adjust the sound file name accordingly
+  
     if languageCode == "ar" {
         
         localizedSoundName += "_ar"
