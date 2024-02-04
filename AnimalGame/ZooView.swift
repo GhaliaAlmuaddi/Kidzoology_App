5337b3f2de70ca1a824ea8144326efac59cfa627
shomokh123 <ip5115@outlook.sa>
2024-02-04 23:15:47 +0300

    zoo view

---
+++ AnimalGame/ZooView.swift
@@ -0,0 +1,18 @@
+//
+//  ZooView.swift
+//  AnimalGame
+//
+//  Created by shomokh aldosari on 23/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ZooView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZooView()
+}
